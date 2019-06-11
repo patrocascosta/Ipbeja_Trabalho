a5ed44a6602c39e3aac2709819c265263124542d
GuilhermeDoutor <49200101+GuilhermeDoutor@users.noreply.github.com>
2019-06-11 11:47:51 +0100

    g

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/Vistos.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/Vistos.java
@@ -1,7 +1,20 @@
 package pt.ipbeja.tvtime.model;
 
+<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+=======
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Vistos {
+    @PrimaryKey(autoGenerate = true)
+    private long idUser;
+    private long idEpisodio;
+    private boolean visto;
+    private String minutos;
+>>>>>>> 1c3c38473ed8268dd36cd59a0ae511b63cd5eaec
 
 import pt.ipbeja.tvtime.R;
 
