73d600a5c4dfb30d811403fadda693475ae11598
patrocascosta <patrocascosta@gmail.com>
2019-06-11 13:48:06 +0100

    Tentativa erro

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/SignUp.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/SignUp.java
@@ -1,14 +1,14 @@
 package pt.ipbeja.tvtime.model;
 
-        import android.content.Context;
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.EditText;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-        import pt.ipbeja.tvtime.R;
+import pt.ipbeja.tvtime.R;
 
 public class SignUp extends AppCompatActivity {
 
