bb13226f402fe91d9de2aee283fc34a1e838aa1c
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-06-12 15:03:24 +0100

    Revert "c"
    This reverts commit 87e82aebba739a169afe1af53698e2a4fe9a2251.

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/Vistos.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/Vistos.java
@@ -1,21 +1,24 @@
 package pt.ipbeja.tvtime.model;
 
-
+<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+=======
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
-import pt.ipbeja.tvtime.R;
-
 @Entity
-public class Vistos extends AppCompatActivity {
+public class Vistos {
     @PrimaryKey(autoGenerate = true)
     private long idUser;
     private long idEpisodio;
     private boolean visto;
     private String minutos;
+>>>>>>> 1c3c38473ed8268dd36cd59a0ae511b63cd5eaec
+
+import pt.ipbeja.tvtime.R;
+
+public class Vistos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
