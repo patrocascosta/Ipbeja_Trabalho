93ac9ad7e654af9ed388b4fca5e8ed597d0f299d
patrocascosta <patrocascosta@gmail.com>
2019-06-12 11:32:51 +0100

    Long_in e Sing up a funcionar

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/AppDatabase.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/model/AppDatabase.java
@@ -1,11 +1,13 @@
 package pt.ipbeja.tvtime.model;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
-@Database(version = 1, entities = {User.class})
+@Database(entities = {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDAO getUserDAO();
@@ -19,6 +21,17 @@ public abstract class AppDatabase extends RoomDatabase {
                 AppDatabase.class,
                 "UserDB")
                 .allowMainThreadQueries()
+
+                // inserir coisas na base de dados
+                .addCallback(new Callback() {
+                    @Override
+                    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                        super.onCreate(db);
+                        //Meter codigo sql
+                        db.execSQL("");
+
+                    }
+                })
                 .build();
     }
     return INSTANCE;
