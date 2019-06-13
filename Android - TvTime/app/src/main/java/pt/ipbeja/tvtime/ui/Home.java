26d97d29c06e8e0d1a0f84ae754941409cb7929b
patrocascosta <patrocascosta@gmail.com>
2019-06-13 12:24:47 +0100

    Tentativa

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/Home.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/Home.java
@@ -4,10 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import pt.ipbeja.tvtime.R;
+import pt.ipbeja.tvtime.model.Series;
 
 public class Home extends AppCompatActivity {
+
+    public static final String EXTRA_ID = "Id";
+
+    private ImageView imageView;
+    private long id ;
+    private Series series;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, Home.class);
@@ -18,6 +26,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.imageView = findViewById(R.id.image1);
 
 
 
