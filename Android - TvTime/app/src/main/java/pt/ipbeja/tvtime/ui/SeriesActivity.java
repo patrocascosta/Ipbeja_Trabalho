0e430645470f82372fdb91c947a2791c1d90dc8c
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-07-10 02:34:26 +0100

    c

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/SeriesActivity.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/SeriesActivity.java
@@ -1,9 +1,30 @@
 package pt.ipbeja.tvtime.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import pt.ipbeja.tvtime.R;
 
 public class SeriesActivity extends Activity {
 
+private ImageView imageViewSeries;
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, SeriesActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series);
+
+        this.imageViewSeries = findViewById(R.id.imageViewSerie);
+
+    }
 
 
 }
