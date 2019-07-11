8828502d2ae62609a3d461c6fba82ed5d27d133a
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-07-11 19:24:33 +0100

    c

--- Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/FavoritosAdapter.java
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/FavoritosAdapter.java
@@ -1,42 +1,60 @@
 package pt.ipbeja.tvtime.ui;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import pt.ipbeja.tvtime.R;
 import pt.ipbeja.tvtime.model.Favorito;
-import pt.ipbeja.tvtime.model.Serie;
 
 public class FavoritosAdapter extends BaseAdapter {
 
     private List<Favorito> favoritoList;
     private Context context;
 
-    public FavoritosAdapter (List<Favorito> favoritoList, Context context) {
+    public FavoritosAdaptor(List<Favorito> favoritoList, Context context){
         this.favoritoList = favoritoList;
         this.context = context;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return this.favoritoList.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Favorito getItem(int position) {
+        return this.favoritoList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return getItem(position).getIdFavoritos();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(this.context).inflate(R.layout.listavistos, parent, false);
+        }
+
+        Favorito favorito = this.getItem(position);
+
+        ImageView imageView = convertView.findViewById(R.id.imageView6);
+        ImageView imageView2 = convertView.findViewById(R.id.imageView4);
+
+
+        Glide.with(this.context).load(favorito.getImagem()).into(imageView);
+        Glide.with(this.context).load(favorito.getImagem()).into(imageView2);
+
+        return convertView;
     }
 }
