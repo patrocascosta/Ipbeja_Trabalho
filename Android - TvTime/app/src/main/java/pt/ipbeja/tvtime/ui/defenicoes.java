8577b69956d188d4a3037ddb42324f66e0c228d0
godofanimes <46845347+godofanimes@users.noreply.github.com>
2019-10-02 11:28:05 +0100

    a

---
+++ Android - TvTime/app/src/main/java/pt/ipbeja/tvtime/ui/defenicoes.java
@@ -0,0 +1,15 @@
+package pt.ipbeja.tvtime.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pt.ipbeja.tvtime.R;
+
+public class defenicoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defenicoes);
+    }
+}
